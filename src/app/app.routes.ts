1f5a4f05927111f79f4345e98439b92dd4a0b5b7
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-23 13:07:57 +0100

    New components generated for Planner module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from "@home/components/home/home.component";
+import { HomeComponent } from "@home/components/home/home.component";
+import { PlannerComponent } from "@planner/components/planner/planner.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'home'}
+  { path: 'planner', component: PlannerComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' }
 ];
---
+++ src/app/planner/components/planner/planner.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalendarComponent } from "@planner/components/calendar/calendar.component";
+
+@Component({
+  selector: 'app-planner',
+  standalone: true,
+  imports: [CommonModule, CalendarComponent],
+  templateUrl: './planner.component.html',
+  styleUrl: './planner.component.scss'
+})
+export class PlannerComponent {
+
+}
