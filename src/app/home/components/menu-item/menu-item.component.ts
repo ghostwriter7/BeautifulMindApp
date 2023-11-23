1522e480463d52361fa6c9b79b6f8a8f8b1d85fa
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-23 09:08:44 +0100

    Adding Material Card cmp to MenuItem

--- src/app/home/components/menu-item/menu-item.component.ts
+++ src/app/home/components/menu-item/menu-item.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuItem } from "@home/interfaces";
 import { RouterLink } from "@angular/router";
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-menu-item',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatCardModule],
   templateUrl: './menu-item.component.html',
   styleUrl: './menu-item.component.scss'
 })
