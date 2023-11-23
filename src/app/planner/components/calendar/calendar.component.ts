7ffeda8a1c0886a4f2890b33d1e32672510730e7
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-23 20:57:25 +0100

    Generating a Calendar Grid for the current month

--- src/app/planner/components/calendar/calendar.component.ts
+++ src/app/planner/components/calendar/calendar.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
+  days: { date: Date, positionClass: string } [];
 
+  ngOnInit(): void {
+    const now = new Date();
+    const currentYear = now.getFullYear();
+    const currentMonth = now.getMonth();
+    const lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0);
+    const lastDateIndex = lastDateOfMonth.getDate();
+
+    this.days = new Array(lastDateIndex).fill(0).map((_, index) => {
+      const date = new Date(currentYear, currentMonth, index + 1);
+      const dayIndex = date.getDay() + 1;
+      return { date, positionClass: `day-${dayIndex}` }
+    });
+  }
 }
