89554ac5c2840614be55eb76547cda3a314caf93
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-23 12:24:39 +0100

    Tweaking colors, button added to the menu item tile

--- src/app/home/components/menu-item/menu-item.component.ts
+++ src/app/home/components/menu-item/menu-item.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MenuItem } from "@home/interfaces";
 import { RouterLink } from "@angular/router";
 import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-menu-item',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatCardModule],
+  imports: [CommonModule, RouterLink, MatCardModule, MatButtonModule],
   templateUrl: './menu-item.component.html',
   styleUrl: './menu-item.component.scss'
 })
