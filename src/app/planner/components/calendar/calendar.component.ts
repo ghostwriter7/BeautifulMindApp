31d0f6c9ab13fdd5397f830dd2f5bd7695bc963e
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-23 21:50:45 +0100

    Displaying action tools in the toolbar above the calendar

--- src/app/planner/components/calendar/calendar.component.ts
+++ src/app/planner/components/calendar/calendar.component.ts
@@ -3,17 +3,23 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from "@angular/material/card";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSelectModule } from "@angular/material/select";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatDividerModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatDividerModule, MatButtonModule, MatToolbarModule, MatSelectModule, MatIconModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent implements OnInit {
   days: { dayOfMonth: number, date: Date, positionClass: string } [];
   daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  label: string;
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  years: number[] = new Array(10).fill(0).map((_, index) => 2023 + index);
 
   ngOnInit(): void {
     const now = new Date();
@@ -27,5 +33,6 @@ export class CalendarComponent implements OnInit {
       const dayIndex = date.getDay() + 1;
       return { dayOfMonth: date.getDate(), date, positionClass: `calendar__day-${dayIndex}` }
     });
+    this.label = `${now.toLocaleString('default', { month: 'long', year: 'numeric' })}`
   }
 }
