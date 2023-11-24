89b5f78c9f21fb45426e7076452aef5919fe273d
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-24 08:59:47 +0100

    Added form controls for year and month fields

--- src/app/planner/components/calendar/calendar.component.ts
+++ src/app/planner/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from "@angular/material/card";
 import { MatDividerModule } from "@angular/material/divider";
@@ -6,11 +6,12 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSelectModule } from "@angular/material/select";
 import { MatIconModule } from "@angular/material/icon";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatDividerModule, MatButtonModule, MatToolbarModule, MatSelectModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatDividerModule, MatButtonModule, MatToolbarModule, MatSelectModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
@@ -18,7 +19,12 @@ export class CalendarComponent implements OnInit {
   days: { dayOfMonth: number, date: Date, positionClass: string } [];
   daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   label: string;
-  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  monthFormControl = new FormControl<number>(null);
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'].map((label, index) => ({
+    label,
+    value: index
+  }));
+  yearFormControl = new FormControl<number>(null);
   years: number[] = new Array(10).fill(0).map((_, index) => 2023 + index);
 
   ngOnInit(): void {
@@ -27,6 +33,9 @@ export class CalendarComponent implements OnInit {
     const currentMonth = now.getMonth();
     const lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0);
     const lastDateIndex = lastDateOfMonth.getDate();
+
+    this.monthFormControl.setValue(currentMonth);
+    this.yearFormControl.setValue(currentYear);
 
     this.days = new Array(lastDateIndex).fill(0).map((_, index) => {
       const date = new Date(currentYear, currentMonth, index + 1);
