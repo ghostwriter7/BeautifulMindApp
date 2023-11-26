3d7d019abd1d0860697a87bea3b3e3ebaeb441c8
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-26 22:51:30 +0100

    Setup for building Event form

--- src/app/planner/components/calendar/event-form/event-form.component.ts
+++ src/app/planner/components/calendar/event-form/event-form.component.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-event-form',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './event-form.component.html',
   styleUrl: './event-form.component.scss'
 })
 export class EventFormComponent {
+  form = new FormGroup({
+    title: new FormControl<string>(null, Validators.required),
+    description: new FormControl<string>(null),
+    startDateTime: new FormControl<string>(null, Validators.required),
+    endDateTime: new FormControl<string>(null, Validators.required),
+    location: new FormControl<string>(null, Validators.required)
+  });
 }
