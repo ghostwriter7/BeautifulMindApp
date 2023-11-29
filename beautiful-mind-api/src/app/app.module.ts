fca55f707d62aadcae1f1ae33115e256f5c0b2a9
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-29 10:56:51 +0100

    Sample LoggingMiddleware added for learning purposes

--- beautiful-mind-api/src/app/app.module.ts
+++ beautiful-mind-api/src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventModule } from "@event/event.module";
+import { LoggerMiddleware } from "./logger.middleware";
 
 @Module({
   imports: [EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('event');
+  }
+
 }
