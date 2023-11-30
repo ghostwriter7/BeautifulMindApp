15840ba2bffb15df61f3ad0891d799f2704ba3e5
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-30 21:25:36 +0100

    Initial route for "login" added to AuthController

--- beautiful-mind-api/src/auth/auth.controller.ts
+++ beautiful-mind-api/src/auth/auth.controller.ts
@@ -1,5 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
+
+  constructor(private authService: AuthService) {
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post("login")
+  signIn(@Body() signInDto: Record<'username' | 'password', string>): any {
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
 }
