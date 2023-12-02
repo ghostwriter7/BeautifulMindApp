8f6868953aeaf53a6a5477b1a454425567a0de44
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-12-02 11:20:50 +0100

    Simple functional guard added

--- beautiful-mind-app/src/app/app.routes.ts
+++ beautiful-mind-app/src/app/app.routes.ts
@@ -1,11 +1,17 @@
-import { Routes } from '@angular/router';
+import { CanActivateFn, Router, Routes, UrlTree } from '@angular/router';
 import { HomeComponent } from "@home/components/home/home.component";
 import { PlannerComponent } from "@planner/components/planner/planner.component";
 import { AuthComponent } from "@auth/auth.component";
+import { inject } from "@angular/core";
+import { UserService } from "@auth/services/user.service";
+
+const authGuardFn: CanActivateFn = (): boolean | UrlTree => {
+    return !!inject(UserService).accessToken || inject(Router).createUrlTree(['auth']);
+};
 
 export const routes: Routes = [
-  { path: 'auth', component: AuthComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'planner', component: PlannerComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'auth' }
+    { path: 'auth', component: AuthComponent },
+    { path: 'home', component: HomeComponent, canActivate: [authGuardFn] },
+    { path: 'planner', component: PlannerComponent, canActivate: [authGuardFn] },
+    { path: '', pathMatch: 'full', redirectTo: 'auth' }
 ];
