da4da535f67b1f4981eef743c60c3c1ed08abff3
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-12-02 10:55:13 +0100

    CORS enabled for localhost

--- beautiful-mind-api/src/main.ts
+++ beautiful-mind-api/src/main.ts
@@ -9,15 +9,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = 'api';
-    app.setGlobalPrefix(globalPrefix);
-    app.useGlobalPipes(new ValidationPipe());
-    const port = process.env['PORT'] || 3000;
-    await app.listen(port);
-    Logger.log(
-        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-    );
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.enableCors({
+    origin: ['http://localhost:4200']
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  const port = process.env['PORT'] || 3000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
